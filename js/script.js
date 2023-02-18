9ec9dc59e5e85ee49d67ee8d68a150d3e5dcef84
Walter-Schwartzer <112778029+Walter-Schwartzer@users.noreply.github.com>
2023-02-18 10:26:28 -0300

    m

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.getElementById("icon_bars").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+    document.querySelector(".nav-links").classList.toggle("mostar_menu")
+}
