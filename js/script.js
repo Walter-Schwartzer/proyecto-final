e1cced5fec9b3eb95c23c4b17e5e3624463c312d
Walter-Schwartzer <112778029+Walter-Schwartzer@users.noreply.github.com>
2023-02-19 11:39:26 -0300

    responsive

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
-document.getElementById("icon_bars").addEventListener("click", mostrar_menu);
+// document.getElementById("icon_bars").addEventListener("click", mostrar_menu);
 
-function mostrar_menu(){
-    document.querySelector(".nav-links").classList.toggle("mostar_menu")
-}
+// function mostrar_menu(){
+//     document.querySelector(".nav-links").classList.toggle("mostar_menu")
+// }
+
+//alert("Bienvenido a mi pagina web")
